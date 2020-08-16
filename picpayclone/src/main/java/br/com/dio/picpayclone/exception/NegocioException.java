a4fc9ae0a58f9a6f8beea1bc8fc9566bee6aa1ee
Alessandro <sandromuniz27@hotmail.com>
2020-08-16 17:31:33 -0400

    adicionado arquitetura e principais funcionalidades do picpayclone

---
+++ picpayclone/src/main/java/br/com/dio/picpayclone/exception/NegocioException.java
@@ -0,0 +1,14 @@
+package br.com.dio.picpayclone.exception;
+
+public class NegocioException extends RuntimeException {
+
+    private static final long serialVersionUID = -7507642233804283142L;
+
+    public NegocioException(String mensagem) {
+        super(mensagem);
+    }
+
+    public NegocioException(String mensagem, Exception exception) {
+        super(mensagem, exception);
+    }
+}
---
+++ picpayclone/src/main/java/br/com/dio/picpayclone/service/ICartaoCreditoService.java
@@ -0,0 +1,8 @@
+package br.com.dio.picpayclone.service;
+
+import br.com.dio.picpayclone.dto.CartaoCreditoDTO;
+
+public interface ICartaoCreditoService {
+
+    CartaoCreditoDTO salvar(CartaoCreditoDTO cartaoCreditoDTO);
+}
