944428be0284d0e03b8335f45a00f447a0496309
Alessandro <sandromuniz27@hotmail.com>
2020-08-17 19:52:34 -0400

    Ajustes no pacote na classe principal do spring

--- picpayclone/src/main/java/br/com/dio/picpayclone/repository/UsuarioRepository.java
+++ picpayclone/src/main/java/br/com/dio/picpayclone/repository/UsuarioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Usuario findByLogin(String login);
